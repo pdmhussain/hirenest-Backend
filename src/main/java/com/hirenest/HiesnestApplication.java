0eb2962af9fa7d266d71c61c0bb643964ec01e01
Gangadhar050 <gangadharhosamani050@gmail.com>
2026-09-25 12:13:02 +0530

    Initial commit

---
+++ src/main/java/com/hirenest/HiesnestApplication.java
@@ -0,0 +1,13 @@
+package com.hirenest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiesnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiesnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirenest/HiesnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiesnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
